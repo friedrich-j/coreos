8f91fb57ba120e3ce357b754a1c26ce4e31c6f4a
friedrich-j <friedrich-j@users.noreply.github.com>
2017-10-09 19:22:42 +0200

    creation phase

--- training/install.sh
+++ training/install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm training.ignition coreos-install
 wget -q -O coreos-install https://raw.github.com/coreos/init/master/bin/coreos-install || exit 1
 wget -q -O training.ignition https://raw.githubusercontent.com/friedrich-j/coreos/master/containers/training/training.ignition || exit 1
 echo "Enter the password for user 'core'."
