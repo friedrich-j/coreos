1bfb858cedd917789316052290d2522247dc2800
friedrich-j <friedrich-j@users.noreply.github.com>
2018-09-25 14:19:33 +0200

    changes

--- ignition/install.sh
+++ ignition/install.sh
@@ -2,11 +2,11 @@
 TTY="/dev/`ps -o tty= -p $$`"
 rm *.ign coreos-install
 read -p "ignition path? " ign_uri < $TTY || exit 1
+ign=`basename "$ign"`
 wget -O coreos-install https://raw.github.com/coreos/init/master/bin/coreos-install || exit 1
-wget -O training.ignition https://raw.githubusercontent.com/friedrich-j/coreos/master/ignition/$ign_uri || exit 1
+wget -O "$ign" https://raw.githubusercontent.com/friedrich-j/coreos/master/ignition/$ign_uri || exit 1
 echo "Enter the password for user 'core'."
 hash=`openssl passwd -1 < $TTY` || exit 1
-ign=`basename "$ign"`
 mv "$ign" "$ign.tmp" || exit 1
 awk -v h="$hash" '{ gsub("\\{PASSWORD\\}", h); print }' "$ign.tmp" > "$ign" || exit 1
 sudo coreos-install -d /dev/sda -i "$ign" -o vmware_raw
