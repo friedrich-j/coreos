884df88d37f643b05aea824787167816eace90e8
friedrich-j <friedrich-j@users.noreply.github.com>
2018-09-25 14:23:42 +0200

    changes

--- ignition/install.sh
+++ ignition/install.sh
@@ -2,7 +2,7 @@
 TTY="/dev/`ps -o tty= -p $$`"
 rm *.ign coreos-install
 read -p "ignition path? " ign_uri < $TTY || exit 1
-ign=`basename "$ign"`
+ign=`basename "$ign_uri"`
 wget -O coreos-install https://raw.github.com/coreos/init/master/bin/coreos-install || exit 1
 wget -O "$ign" https://raw.githubusercontent.com/friedrich-j/coreos/master/ignition/$ign_uri || exit 1
 echo "Enter the password for user 'core'."
