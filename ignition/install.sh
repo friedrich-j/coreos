0913881e4198241dd6671fd0b6830248cb1b79eb
friedrich-j <j.friedrich@gmx.net>
2019-01-17 19:12:11 +0100

    fixes

--- ignition/install.sh
+++ ignition/install.sh
@@ -7,7 +7,6 @@ wget -O coreos-install https://raw.github.com/coreos/init/master/bin/coreos-inst
 wget -O "$ign" https://raw.githubusercontent.com/friedrich-j/coreos/master/ignition/$ign_uri || exit 1
 echo "Enter the password for user 'core'."
 hash=`openssl passwd -1 < $TTY` || exit 1
-mv "$ign" "$ign.tmp" || exit 1
 chmod 755 coreos-install
 sed -i "s|PASSWORD|$hash|" "$ign"
 sudo $PWD/coreos-install -d /dev/sda -i "$ign" -o vmware_raw
