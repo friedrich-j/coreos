843e9d95b5c3a78cac4f44c8d2228f2a35c99dbc
friedrich-j <friedrich-j@users.noreply.github.com>
2019-04-28 14:03:48 +0200

    improved automation

--- training/vagrant/README.md
+++ training/vagrant/README.md
@@ -10,8 +10,10 @@ This vagrant files are to install a single-node training kubernetes cluster. Onl
 vagrant up
 vagrant scp :/home/core/.ssh/id_rsa id_rsa
 vagrant scp :/home/core/.ssh/id_rsa.ppk id_rsa.ppk
+vagrant scp :/home/core/.kube/config kube_config
 ```
-- Use file id_rsa for SSH key authentication of Linux ssh commands.
-- Use file id_rsa.ppk for using PuTTY. Add this file to PuTTY's Pageant.
+- Use the file id_rsa for SSH key authentication of Linux ssh commands.
+- Use the file id_rsa.ppk for using PuTTY. Add this file to PuTTY's Pageant.
+- Use the file kube_config for accessing your k8s cluster via kubectl. Execute ```KUBECONFIG=$HOME/kube_config``` for this.
 
-*Note:* The passphrase of the SSH Key file is ```training```
+*Note:* The passphrase of the SSH key file is ```training```
---
+++ training/vagrant/create.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+set -o errexit
+set -o nounset
+
+mkdir -p vm-data
+rm vm-data/* 2>/dev/null && err=$?
+
+vagrant up
+
+rm config.ign.merged config.img config.vmdk k8s-training.ign.merged 2>/dev/null && err=$?
+
+ip=`vagrant ssh -c 'cat /home/core/.ssh/hostOnlyIp.txt'`
+
+sed -i'' -e "/^$ip/d" ~/.ssh/known_hosts
+
+#vagrant ssh -c 'cat /home/core/.ssh/id_rsa' > vm-data/id_rsa
+vagrant scp :/home/core/.ssh/id_rsa vm-data/id_rsa
+vagrant scp :/home/core/.ssh/id_rsa.pub vm-data/id_rsa.pub
+vagrant scp :/home/core/.ssh/id_rsa.ppk vm-data/id_rsa.ppk
+vagrant scp :/home/core/.kube/config vm-data/kube_config.orig
+vagrant scp :/home/core/.ssh/hostOnlyIp.txt vm-data/hostOnlyIp.txt
+
+sed -e "s#\\(https://\\)[0-9.]*:#\\1$ip:#g" vm-data/kube_config.orig > vm-data/kube_config
+
+echo
+echo "============================================================================="
+echo "Popular commands:"
+echo
+echo "ssh -i vm-data/id_rsa core@$ip"
+echo
+echo "export KUBECONFIG=$PWD/vm-data/kube_config"
+echo "============================================================================="
+
